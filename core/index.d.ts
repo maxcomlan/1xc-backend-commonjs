b274b5cf801f63b84e14e04907e438dc873878d7
themx05 <maximiliencomlan05@gmail.com>
2020-10-08 20:08:52 +0100

    v2.0.12

--- core/index.d.ts
+++ core/index.d.ts
@@ -185,8 +185,6 @@ export interface Transaction extends Indexable, Insertable {
     status: TxStatus;
     insertedAt: number;
     validatedAt?: number;
-    confirmedAt?: number;
-    paidAt: number;
 }
 export interface ExchangeCalculation {
     source: string;
