194c57c6e8eab1345fbb9ccbaa18a729f90dc758
themx05 <maximiliencomlan05@gmail.com>
2020-10-27 20:29:08 +0100

    added method to authenticate a UAT

--- src/clients/UserServiceClient.ts
+++ src/clients/UserServiceClient.ts
@@ -1,6 +1,7 @@
-import { Customer } from "..";
+import { Customer, AccessToken } from "..";
 import { ServiceMetadata } from "../peers";
 import { JsonServiceClient } from "./JsonServiceClient";
+import Axios from "axios";
 
 export class UserServiceClient extends JsonServiceClient<Customer>{
     static clientMetadata: ServiceMetadata;
@@ -8,6 +9,21 @@ export class UserServiceClient extends JsonServiceClient<Customer>{
 
     constructor(){
         super(UserServiceClient.url, UserServiceClient.clientMetadata);
+    }
+
+    async getTokenData(token: string): Promise<{token: AccessToken, userId: string, firstName: string, lastName: string}>{
+        return Axios.get(
+            `${this.url}/uat/${token}`,
+            {
+                withCredentials: true,
+                headers: this.defaultHeaders
+            }
+        )
+        .then((res) => {
+            if(res.data.success){
+                return res.data.data;
+            }
+        })
     }
 }
 
