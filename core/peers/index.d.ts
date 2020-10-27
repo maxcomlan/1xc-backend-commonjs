e62359ad530be9a99532165a6d0f50d35c7bb95e
themx05 <maximiliencomlan05@gmail.com>
2020-10-27 19:01:30 +0100

    Added user access token authentication method

--- core/peers/index.d.ts
+++ core/peers/index.d.ts
@@ -1,4 +1,5 @@
 import { ScopedRole } from "../roles";
+import { AccessToken } from "..";
 export interface ServiceMetadata {
     name: string;
     signature: string;
@@ -8,15 +9,23 @@ export interface ServiceMetadata {
 export declare type TokenType = "user" | "admin";
 export interface TokenData {
     type: TokenType;
+    method: "ses" | "uat";
 }
-export interface UserTokenData extends TokenData {
+export interface UserSESTokenData extends TokenData {
     type: "user";
+    method: "ses";
     userId: string;
     firstName: string;
     lastName: string;
 }
+export interface UserUATTokenData extends TokenData {
+    type: "user";
+    method: "uat";
+    token: AccessToken;
+}
 export interface AdminTokenData extends TokenData {
     type: "admin";
+    method: "ses";
     userId: string;
     firstName: string;
     lastName: string;
@@ -33,7 +42,7 @@ interface ServicePeer extends BasePeer {
 }
 interface UserPeer extends BasePeer {
     type: "user";
-    data: UserTokenData;
+    data: UserSESTokenData | UserUATTokenData;
 }
 interface AdminPeer extends BasePeer {
     type: "admin";
--- src/peers/index.ts
+++ src/peers/index.ts
@@ -1,4 +1,5 @@
 import { ScopedRole } from "../roles";
+import { AccessToken } from "..";
 
 export interface ServiceMetadata{
     name: string,
@@ -7,21 +8,30 @@ export interface ServiceMetadata{
     port: number
 }
 
-export type TokenType = "user" | "admin";
+export type TokenType = "user" | "admin"; // uat = User Access Token; ses = Session
 
 export interface TokenData{
     type: TokenType;
+    method: "ses" | "uat"; // Authentication through session or authentication through user access token
 }
 
-export interface UserTokenData extends TokenData{
+export interface UserSESTokenData extends TokenData{
     type: "user";
+    method: "ses";
     userId: string;
     firstName: string;
     lastName: string;
 }
 
+export interface UserUATTokenData extends TokenData{
+    type: "user";
+    method: "uat";
+    token: AccessToken;
+}
+
 export interface AdminTokenData extends TokenData{
     type: "admin";
+    method: "ses";
     userId: string;
     firstName: string;
     lastName: string;
@@ -42,7 +52,7 @@ interface ServicePeer extends BasePeer{
 
 interface UserPeer extends BasePeer{
     type: "user";
-    data: UserTokenData;
+    data: UserSESTokenData | UserUATTokenData;
 }
 
 interface AdminPeer extends BasePeer{
