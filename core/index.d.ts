9f17b87e3876d37dfbbafd07862633611bda285e
themx05 <maximiliencomlan05@gmail.com>
2021-03-31 15:58:45 +0100

    fixed vulns

--- core/index.d.ts
+++ core/index.d.ts
@@ -149,7 +149,7 @@ export interface Method extends Indexable, Insertable, Patchable {
     color?: string;
     allowSell: boolean;
     allowBuy: boolean;
-    availableAmount: number;
+    availableAmount?: number;
     addedAt?: number;
     details: MethodDetails;
 }
